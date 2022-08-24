22da835bb2e72fb0ebfc646d76bfbd2d92fde156
a-flower03 <111801629+a-flower03@users.noreply.github.com>
2022-08-24 12:18:22 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(100)
+})
