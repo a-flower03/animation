40a2d89f2e0327cfd0ec986d695faa3bff8a08ae
a-flower03 <111801629+a-flower03@users.noreply.github.com>
2022-08-29 12:26:01 -0600

    new code for pseudo assignment

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-basic.forever(function () {
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
     basic.showIcon(IconNames.Heart)
-    basic.pause(200)
-    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
     basic.pause(100)
-    basic.showIcon(IconNames.SmallDiamond)
-    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
